e46b50608ab00ffaf6fa0e92909e2c0581bc0f04
mbz <30321314+mbz4@users.noreply.github.com>
2023-03-09 12:30:39 +0200

    twython expanded

--- week3/twitter_demo.py
+++ week3/twitter_demo.py
@@ -1,3 +1,20 @@
+# from twython import TwythonStreamer
+# class MyStreamer(TwythonStreamer):
+#         count=0
+#         def on_success(self,data):
+#                 if 'text' in data:
+#                         self.count=self.count+1
+#                 if self.count>2:
+#                         print("Ian G. Harris is popular")
+# string="Ian G. Harris"
+# C_KEY=""
+# C_SECRET=""
+# A_TOKEN=""
+# A_SECRET=""
+
+# stream=MyStreamer(C_KEY,C_SECRET,A_TOKEN,A_SECRET)
+# stream.statuses.filter(track=string)
+
 from twython import TwythonStreamer
 class MyStreamer(TwythonStreamer):
         count=0
@@ -7,10 +24,10 @@ class MyStreamer(TwythonStreamer):
                 if self.count>2:
                         print("Ian G. Harris is popular")
 string="Ian G. Harris"
-C_KEY=""
-C_SECRET=""
-A_TOKEN=""
-A_SECRET=""
+C_KEY="2aiLH5K4v0wcpDXu3nujLTn9J"
+C_SECRET="9mmE7AoMcLZbHn1YYMIB8cOax3fVDvKrwJCfMTpV00XBRkjyUz"
+A_TOKEN="1015534523847606273-ube7Xh0azaGsQ9OgiLntTu0fggFKwQ"
+A_SECRET="zHg2OBUIifwf2yD06QuoLYV3qReWYXbCihL1B0CyW8A8B"
 
 stream=MyStreamer(C_KEY,C_SECRET,A_TOKEN,A_SECRET)
 stream.statuses.filter(track=string)
